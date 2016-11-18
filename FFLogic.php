af9da75e9cb822e3628031bf68b3ef4476bafe93
Altay <altayadademir@gmail.com>
2016-11-18 22:43:08 +0000

    Radius checking works

--- FFLogic.php
+++ FFLogic.php
@@ -1,10 +1,37 @@
 <?php
 
-require($_SERVER['DOCUMENT_ROOT'] . '/app/DB/DB.php');
+$host= 'mysql.hostinger.co.uk';
+$username = 'u439981189_main';
+$password = '041195';
+$db= 'u439981189_main';
+$conn = mysqli_connect($host, $username, $password, $db);
+
 
 // Selects the 50 closest ships within a radius of the specifies coordinates in KMs
 function determineRescuers($lat, $lng, $radius){
-    $query = 'SELECT id, ( 6371 * acos( cos( radians(' . $lat .') ) * cos( radians( lat ) ) * cos( radians( lng ) - radians(' . $lng . ') ) + sin( radians(' . $lat .') ) * sin( radians( lat ) ) ) ) AS distance FROM markers HAVING distance < ' . $radius .' ORDER BY distance LIMIT 0 , 50';
-}
+	
+	global $conn;
+	
+    $sql = 'SELECT id, ( 6371 * acos( cos( radians(' . $lat .') ) * cos( radians( lat ) ) * cos( radians( lng ) - radians(' . $lng . ') ) + sin( radians(' . $lat .') ) * sin( radians( lat ) ) ) ) AS distance FROM alerts HAVING distance < ' . $radius .' ORDER BY distance LIMIT 0 , 50';
 
-?>
+	if ($result = $conn->query($sql)) {
+			
+			if ($result->num_rows > 0) {
+				// output data of each row
+				while($row = $result->fetch_assoc()) {
+					echo "id: " . $row["id"];
+				}
+			} else {
+				echo "0 results";
+			}
+			
+			
+		} else {
+			echo 'Fail';
+		}	
+	
+}
+	
+
+determineRescuers('50.740736', '-1.823260', 5.1);
+	
