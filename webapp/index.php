74659e9549b75559dcb1d25739a00bf0100ff559
ricky3997 <riccardo@broggi.co.uk>
2016-11-19 10:05:34 +0000

    Increased UI Functionality to notify users

--- webapp/index.php
+++ webapp/index.php
@@ -7,25 +7,27 @@ $gcm = new GCM();
 $vessels = array();
 $emergency = array();
 
-$sql = "SELECT lng,lat FROM users WHERE status='active' ";
+$sql = "SELECT lng,lat,token FROM users WHERE status='active' ";
 
 if ($result = $conn->query($sql)) {
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
             array_push($vessels, $row['lng']);
             array_push($vessels, $row['lat']);
+            array_push($vessels, $row['token']);
         }
     }
 }
 
 
-$sql = "SELECT lng,lat FROM users WHERE status='emergency' ";
+$sql = "SELECT lng,lat,token FROM users WHERE status='emergency' ";
 
 if ($result = $conn->query($sql)) {
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
             array_push($emergency, $row['lng']);
             array_push($emergency, $row['lat']);
+            array_push($emergency, $row['token']);
         }
     }
 }
@@ -34,6 +36,7 @@ if ($result = $conn->query($sql)) {
 <!DOCTYPE html>
 <html>
 <head>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <style>
         #map {
             height: 600px;
@@ -42,31 +45,57 @@ if ($result = $conn->query($sql)) {
     </style>
 </head>
 <body>
-<center>
-    <h2>MRCC & RLNI Monitoring System</h3></center>
-<div id="map"></div>
+    <h2>MRCC & RLNI Monitoring System</h3>
+
+	</center>
+	<div class="col-md-10">
+	<div id="map"></div></div>
+	<div class="col-md-2">
+	<h1>Menu</h1>
+	<a href="/app/GCM/test.php?update=1"><button>Update Vessel Locations</button></a>
+        <form action="/app/GCM/test.php" method="post">
+        <br>
+        <div id="messageVessel"></div>
+        <hr>
+        Send Direct Message to the selected vessel.
+		<input type="text" name="notify" value="true" hidden>
+        <input type="text" name="message" placeholder="Message for the vessel">
+        <input type="hidden" value="" name="vessel" id="tokenForMessage" >
+        <input value="Send" type="submit" style="margin-top: 5px;">
+        </form>
+	</div>
+	</div>
 <script> 
 function initMap(){
-    var initialLocation = {lat: -6.813111, lng: 39.304491};
+    var emergency = <?php echo json_encode($emergency); ?>;
+    var vessels = <?php echo json_encode($vessels); ?>;
+    var initialLocation;
+
+    if((parseFloat(emergency[0]) != null && parseFloat(emergency[0]) != null)){
+        initialLocation = {lat: parseFloat(emergency[0]), lng: parseFloat(emergency[1])};
+        }
+    else {
+        initialLocation = {lat: 0, lng: 0};
+        }
 
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 15,
+        zoom: 6,
         center: initialLocation
     });
 
-    var vessels = <?php echo json_encode($vessels); ?>;
-        for(i=0; i<vessels.length;i=i+2){
+        for(i=0; i<vessels.length;i=i+3){
             var lng = parseFloat(vessels[i]);
             var lat = parseFloat(vessels[i+1]);
-            createMarker(lat,lng,map);
+            var token = (vessels[i+2]);
+            createMarker(lat,lng,token,map);
     }
 
-    var emergency = <?php echo json_encode($emergency); ?>;
-        for(i=0; i<emergency.length;i=i+2){
+        for(i=0; i<emergency.length;i=i+3){
             var lng = parseFloat(emergency[i]);
             var lat = parseFloat(emergency[i+1]);
-            createEmergencyMarker(lat,lng,map);
-            createEmergencyRadius(lat,lng,map);
+            var token = (emergency[i+2]);
+            createEmergencyMarker(lng,lat,token,map);
+            createEmergencyRadius(lng,lat,token,map);
         }
 
     //
@@ -74,21 +103,31 @@ function initMap(){
 
 
 
-function createMarker(lng,lat,map){
+function createMarker(lng,lat,token,map){
     var location = {lat: lat, lng: lng};
     var marker = new google.maps.Marker({
         position: location,
         map: map,
+        title: token,
         icon: 'icon.png'
     });
+     marker.addListener('click', function() {
+              document.getElementById('tokenForMessage').value = '';
+              document.getElementById('tokenForMessage').value = marker.title;
+            });
 }
-function createEmergencyMarker(lng,lat,map){
+function createEmergencyMarker(lng,lat,token,map){
     var location = {lat: lat, lng: lng};
     var marker = new google.maps.Marker({
         position: location,
         map: map,
+        title: token,
         icon: 'http://maps.google.com/mapfiles/kml/shapes/caution.png'
     });
+     marker.addListener('click', function() {
+              document.getElementById('tokenForMessage').value = '';
+              document.getElementById('tokenForMessage').value = marker.title;
+            });
 }
 
 function createEmergencyRadius(lng,lat,map){
